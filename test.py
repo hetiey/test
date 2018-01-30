e09a0cbc6d748a1f86e944125f97bec737326a24
zla <125839560@qq.com>
2018-01-30 17:04:46 +0800

    first commit

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import tensorflow as tf
 import numpy as np
 z=np.random.randint(0,10,size=[10])
 x=np.random.rand(10)
+xx = 1-x
+xxx = xx*xx
 y=tf.one_hot(z,10,on_value=1,off_value=None,axis=0)
 with tf.Session()as sess:
     print(z)
-    print(1-x)
+    print(x)
+    print(xx)
+    print(xxx)
     print(sess.run(y))
