7035f8212301d3f4b6940b9dcc6e391e6d1b4a52
zla <125839560@qq.com>
2018-01-30 17:14:59 +0800

    first commit

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ x=np.random.rand(10)
 xx = 1-x
 xxx = xx*xx
 y=tf.one_hot(z,10,on_value=1,off_value=None,axis=0)
+yy = y*x
+yyy = xxx*y*x
 with tf.Session()as sess:
     print(z)
-    print(x)
-    print(xx)
-    print(xxx)
     print(sess.run(y))
+    print(sess.run(yy))
+    print(sess.run(yyy))
