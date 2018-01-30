3b3a425e74d772faada1609fd3504d5f6c3f7e65
zla <125839560@qq.com>
2018-01-30 17:25:31 +0800

    first commit

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 import numpy as np
 z=np.random.randint(0,10,size=[10])
-x=np.random.rand(10)
-xx = 1-x
-xxx = xx*xx
+a = tf.Variable(tf.random_normal([10,1],seed=1))
 y=tf.one_hot(z,10,on_value=1,off_value=None,axis=0)
-yy = y*x
-yyy = xxx*y*x
+aa = 1-a
+aaa = aa*aa
+yy = y*a
+yyy = aaa * y * a
+init = tf.global_variables_initializer()
 with tf.Session()as sess:
     print(z)
-    print(x)
-    print(yy)
+    print(sess.run(a))
+    print(sess.run(aa))
+    print(sess.run(aaa))        
     print(sess.run(y))
     print(sess.run(yy))
     print(sess.run(yyy))
