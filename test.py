3986a0cc50482738b9bca3d45dbf9677657fd4e2
zla <125839560@qq.com>
2018-01-30 16:59:13 +0800

    first commit

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import numpy as np
 z=np.random.randint(0,10,size=[10])
+x=np.random.rand(0,1,size=[10])
 y=tf.one_hot(z,10,on_value=1,off_value=None,axis=0)
 with tf.Session()as sess:
     print(z)
+    print(x)
     print(sess.run(y))
